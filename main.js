89a31125b1aac46542757f13cde2c5e8dd6eb57d
mosala123 <149291901+mosala123@users.noreply.github.com>
2024-04-27 13:31:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+//  
+
  
-$(document).ready(function(){
-  $('.navbar-toggler').click(function(){
-    $('.navbar-collapse').toggleClass('show');
+document.addEventListener("DOMContentLoaded", function() {
+  let items = document.querySelectorAll(".item");
+
+  items.forEach(function(item) {
+    let icon = item.querySelector("i");
+    let paragraph = item.querySelector("p");
+    let subHeading = item.querySelector("h3");
+
+    icon.addEventListener("click", function() {
+      if (icon.classList.contains("icon-plus")) {
+        icon.classList.remove("icon-plus");
+        icon.classList.add("icon-minus");
+        subHeading.classList.remove("hidden");
+      } else {
+        icon.classList.remove("icon-minus");
+        icon.classList.add("icon-plus");
+        subHeading.classList.add("hidden");
+      }
+    });
   });
 });
  
